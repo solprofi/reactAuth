b7dc210a45df6e607aabb784395318a067356c85
Roman Sologor <solohor.roman@gmail.com>
2019-03-27 20:42:16 +0100

    set up redux

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route } from 'react-router-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
 import App from './components/App';
 import Welcome from './components/Welcome';
 import SignUp from './components/auth/SignUp';
+import rootReducer from './reducers';
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App>
-      <Route exact path='/' component={Welcome} />
-      <Route exact path='/signUp' component={SignUp} />
-    </App>
-  </BrowserRouter>, document.getElementById('root'));
+  <Provider store={createStore(rootReducer, {})}>
+    <BrowserRouter>
+      <App>
+        <Route exact path='/' component={Welcome} />
+        <Route exact path='/signUp' component={SignUp} />
+      </App>
+    </BrowserRouter>
+  </Provider>, document.getElementById('root'));
